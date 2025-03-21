b359f13f2f1c54029f838911c63992422acf0298
CJ020328 <jinghangchai80@gmail.com>
2025-03-21 13:38:40 +0800

    Install lodash and fix _.map bug

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // 环境变量配置
-const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000';
+const API_URL = import.meta.env.VITE_API_URL || 'https://dinemap-api.onrender.com/';
 
 export const config = {
   API_URL
